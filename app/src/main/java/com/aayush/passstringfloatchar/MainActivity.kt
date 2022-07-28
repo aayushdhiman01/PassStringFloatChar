e99280966e4130561f004630b04db1b53658179b
aayushdhiman01 <50516628+aayushdhiman01@users.noreply.github.com>
2022-07-28 12:30:06 +0530

    changes

--- app/src/main/java/com/aayush/passstringfloatchar/MainActivity.kt
+++ app/src/main/java/com/aayush/passstringfloatchar/MainActivity.kt
@@ -6,29 +6,23 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.aayush.passstringfloatchar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var String: EditText
-    lateinit var Float: TextView
-    lateinit var Double: EditText
-    lateinit var Char: EditText
-    lateinit var btnPassValue: Button
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        String = findViewById(R.id.etString)
-        Float = findViewById(R.id.etFloat)
-        Double = findViewById(R.id.etDouble)
-        Char = findViewById(R.id.etChar)
-        btnPassValue = findViewById(R.id.pass)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnPassValue.setOnClickListener {
-            if (String.text.toString().isNullOrEmpty()) {
-                String.error = resources.getString(R.string.please_enter_string)
-                String.requestFocus()
+
+        binding.pass.setOnClickListener {
+            if (binding.etString.text.toString().isNullOrEmpty()) {
+                binding.etString.error = resources.getString(R.string.please_enter_string)
+                binding.etString.requestFocus()
             } else {
                 var intent = Intent(this, result::class.java)
-                intent.putExtra("String", String.text.toString())
+                intent.putExtra("String",binding.etString.text.toString())
                 startActivity(intent)
                 finish()
             }
