38b8104b710424225a3f73d8c953e2f9fe4b26f4
aayushdhiman01 <50516628+aayushdhiman01@users.noreply.github.com>
2022-07-28 20:11:08 +0530

    changes

--- app/src/main/java/com/aayush/passstringfloatchar/MainActivity.kt
+++ app/src/main/java/com/aayush/passstringfloatchar/MainActivity.kt
@@ -15,18 +15,46 @@ class MainActivity : AppCompatActivity() {
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         binding.pass.setOnClickListener {
             if (binding.etString.text.toString().isNullOrEmpty()) {
                 binding.etString.error = resources.getString(R.string.please_enter_string)
                 binding.etString.requestFocus()
-            } else {
-                var intent = Intent(this, result::class.java)
-                intent.putExtra("String",binding.etString.text.toString())
-                startActivity(intent)
+            }
+            else if(binding.etFloat.text.toString().isNullOrEmpty()) {
+                binding.etFloat.error = resources.getString(R.string.please_enter_string)
+                binding.etFloat.requestFocus()
+            }
+            else if(binding.etDouble.text.toString().isNullOrEmpty()) {
+                binding.etDouble.error = resources.getString(R.string.please_enter_string)
+                binding.etDouble.requestFocus()
+            }
+            else if(binding.etChar.text.toString().isNullOrEmpty()) {
+                binding.etChar.error = resources.getString(R.string.please_enter_string)
+                binding.etChar.requestFocus()
+            }
+
+            else
+            {
+                var intentS = Intent(this, result::class.java)
+                intentS.putExtra("String",binding.etString.text.toString())
+                startActivity(intentS)
+                finish()
+                var intentF = Intent(this, result::class.java)
+                intentF.putExtra("Float",binding.etFloat.text.toString())
+                startActivity(intentF)
+                finish()
+                var intentD = Intent(this, result::class.java)
+                intentD.putExtra("Double",binding.etDouble.text.toString())
+                startActivity(intentD)
+                finish()
+                var intentC = Intent(this, result::class.java)
+                intentC.putExtra("Char",binding.etString.text.toString())
+                startActivity(intentC)
                 finish()
             }
+
         }
+
 
     }
 }
