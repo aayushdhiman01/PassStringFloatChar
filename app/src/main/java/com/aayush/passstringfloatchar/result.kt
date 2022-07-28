e883040482b0836980088fa35af1c9f5488ac239
aayushdhiman01 <50516628+aayushdhiman01@users.noreply.github.com>
2022-07-28 21:32:49 +0530

    changes

--- app/src/main/java/com/aayush/passstringfloatchar/result.kt
+++ app/src/main/java/com/aayush/passstringfloatchar/result.kt
@@ -7,10 +7,8 @@ import com.aayush.passstringfloatchar.databinding.ActivityMainBinding
 
 class result : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        lateinit var binding: ActivityMainBinding
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_result)
 
         var pString: String=" "
         lateinit  var tvString: TextView
@@ -22,24 +20,24 @@ class result : AppCompatActivity() {
         tvString.setText(pString)
 
 
-        var pFloat: String=" "
-        lateinit  var tvFloat: TextView
-        intent?.let { abc->
-            if (abc.hasExtra("Float"))
-                pFloat=abc.getStringExtra("Float") as String
-        }?:kotlin.run{ }
-        tvFloat=findViewById(R.id.tvFloat)
-        var  rFloat = Integer.parseInt(pFloat)
-        tvFloat.setText(rFloat)
-        
-        var pDouble: String=" "
-        lateinit  var tvDouble: TextView
-        intent?.let { abc->
-            if (abc.hasExtra("Double"))
-                pDouble=abc.getStringExtra("Double") as String
-        }?:kotlin.run{ }
-        tvFloat=findViewById(R.id.tvDouble)
-        var  rDouble = Integer.parseInt(pDouble)
-        tvFloat.setText(rDouble)
+//        var pFloat: String=" "
+//        lateinit  var tvFloat: TextView
+//        intent?.let { abc->
+//            if (abc.hasExtra("Float"))
+//                pFloat=abc.getStringExtra("Float") as String
+//        }?:kotlin.run{ }
+//        tvFloat=findViewById(R.id.tvFloat)
+//        var  rFloat = Integer.parseInt(pFloat)
+//        tvFloat.setText(rFloat)
+//
+//        var pDouble: String=" "
+//        lateinit  var tvDouble: TextView
+//        intent?.let { abc->
+//            if (abc.hasExtra("Double"))
+//                pDouble=abc.getStringExtra("Double") as String
+//        }?:kotlin.run{ }
+//        tvFloat=findViewById(R.id.tvDouble)
+//        var  rDouble = Integer.parseInt(pDouble)
+//        tvFloat.setText(rDouble)
     }
 }
