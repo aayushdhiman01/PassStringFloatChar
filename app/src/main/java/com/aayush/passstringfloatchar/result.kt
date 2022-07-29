32c2829f2ec0440ae7cf4d5a0c397005993a0ced
aayushdhiman01 <50516628+aayushdhiman01@users.noreply.github.com>
2022-07-29 12:25:27 +0530

    changes

--- app/src/main/java/com/aayush/passstringfloatchar/result.kt
+++ app/src/main/java/com/aayush/passstringfloatchar/result.kt
@@ -10,23 +10,18 @@ class result : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-
+        var pFloat: String=" "
         var pString: String=" "
+        lateinit  var tvFloat: TextView
         lateinit  var tvString: TextView
         intent?.let { abc->
             if (abc.hasExtra("String"))
                 pString=abc.getStringExtra("String") as String
-        }?:kotlin.run{ }
-        tvString=findViewById(R.id.tvString)
-        tvString.setText(pString)
-
-
-        var pFloat: String=" "
-        lateinit  var tvFloat: TextView
-        intent?.let { abc->
             if (abc.hasExtra("Float"))
                 pFloat=abc.getStringExtra("Float") as String
         }?:kotlin.run{ }
+        tvString=findViewById(R.id.tvString)
+        tvString.setText(pString)
         tvFloat=findViewById(R.id.tvFloat)
         var  rFloat = Integer.parseInt(pFloat)
         tvFloat.setText(rFloat)
